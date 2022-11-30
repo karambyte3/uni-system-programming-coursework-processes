d63098e499558ccb5e6293ab98c97b8378d47656
Stefan Kalenderov <stefankalenderov@gmail.com>
2022-11-30 21:45:11 +0200

    initial commit

---
+++ FirstProcess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (MemoryMappedFile mmf = MemoryMappedFile.CreateNew("mappedFile", 1000000000))
+        {
+            bool mutexCreated;
+            Mutex mutex = new Mutex(true, "mutex", out mutexCreated);
+            using (MemoryMappedViewStream stream = mmf.CreateViewStream())
+            {
+                BinaryWriter writer = new BinaryWriter(stream);
+                List<string> firstProcessNames = new List<string>
+                { "Stefan Kalenderov", "Vasil Yordanov", "Dimitar Berbatov" };
+
+                 writer.Write(string.Join(", ", firstProcessNames));
+            }
+
+            mutex.ReleaseMutex();
+            Console.WriteLine("Start SecondProcess and press 'ENTER' to proceed.");
+            Console.ReadLine();
+
+            mutex.WaitOne();
+
+            using (MemoryMappedViewStream stream = mmf.CreateViewStream())
+            {
+                BinaryReader reader = new BinaryReader(stream);
+                Console.WriteLine("Names from FirstProcess file: {0}", reader.ReadString());
+                Console.WriteLine("Names from SecondProcess file: {0}", reader.ReadString());
+
+            }
+
+            mutex.ReleaseMutex();
+        }
+    }
+}
---
+++ SecondProcess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("mappedFile"))
+            {
+
+                Mutex mutex = Mutex.OpenExisting("mutex");
+                mutex.WaitOne();
+
+                using (MemoryMappedViewStream stream = mmf.CreateViewStream(43, 0))
+                {
+                    BinaryWriter writer = new BinaryWriter(stream);
+                    List<string> secondProcessNames = new List<string>
+                    { "Georgi Nikolov", "Nevena Petrova", "Yordan Grigorov" };
+
+                    writer.Write(string.Join(", ", secondProcessNames));
+                }
+                mutex.ReleaseMutex();
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Memory-mapped file doesn't exist! Run FirstProcess file first!");
+        }
+    }
+}
